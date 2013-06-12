3e3dd4286ba3eb5802725837a7add245c4a3392d
Talha Maniar <talha.maniar@maniartech.com>
2013-06-12 19:43:44 +0530

    Work done on x.flatten Work on Documentation correct

--- src/x.array.js
+++ src/x.array.js
@@ -18,7 +18,7 @@ this.load = function load(x) {
      * @version 1.0
      */
     x.range = function range(start, stop, step) {
-        var result=[],
+        var result = [],
             i;
 
         for(i = start; step > 0 ? i < stop : i > stop; i += step) {
@@ -41,15 +41,14 @@ this.load = function load(x) {
      * is not provided return the first value of an array
      *
      * @example
-     * var x=[1,2,3,4,5,6];
-     * console.log(x.first(x,2));  // Prints [1, 2]
+     * var firArray = [1,2,3,4,5,6];
+     * console.log(x.first(firArray, 2));  // Prints [1, 2]
      *
      * @version 1.0
      */
-
     x.first = function first(array, n) {
 
-        if( n === undefined) {
+        if(n === undefined) {
             return array[0];
         }
         else {
@@ -72,12 +71,11 @@ this.load = function load(x) {
      * is not provided return the last value of an array
      *
      * @example
-     * var x=[1,2,3,4,5,6];
-     * console.log(x.last(x,2));  // Prints [5,6]
+     * var lasArray = [1,2,3,4,5,6];
+     * console.log(x.last(lasArray, 2));  // Prints [5,6]
      *
      * @version 1.0
      */
-
     x.last = function last(array, n) {
 
         if(n === undefined) {
@@ -93,7 +91,7 @@ this.load = function load(x) {
      * Computes the intersection of array with another array and find the dublicates value
      * from both the arrays.
      *
-     * @function x.intersection(array1,array2 )
+     * @function x.intersection(array1, array2)
      * @params {number} array1 The array1 value in x.intersection.
      * @params {number} array2 The array2 value in x.intersection
      * @returns {Array1, Array2} Both the array intersect and returns an array
@@ -101,21 +99,20 @@ this.load = function load(x) {
      * its shows the error
      *
      * @example
-     * var array1=[1,2,3,4,5,6];
-     * var array2=[3,4,5,8,9]
-     * console.log(x.intersection(array1,array2));  // Prints [3,4,5]
+     * var intrArray1 = [1,2,3,4,5,6];
+     * var intrArray2 = [3,4,5,8,9];
+     * console.log(x.intersection(intrArray1, intrArray2));  // Prints [3,4,5]
      *
      * @version 1.0
      */
-
     x.intersection = function intersection(array1, array2) {
         var retArray = [],
-            i,j;
+            i,j,
+            iLen = array1.length,
+            jLen = array2.length;
 
-        for(i = 0; i < array1.length; i++) {
-
-            for(j = 0; j < array2.length; j++) {
-
+        for(i = 0; i < iLen; i++) {
+            for(j = 0; j < jLen; j++) {
                 if(array1[i] === array2[j]) {
                     retArray.push(array1[i]);
                 }
@@ -125,10 +122,10 @@ this.load = function load(x) {
     };
 
     /**
-     * Returns the index at which the value found in the array. If the value provided
-     * in the array,then it is true statement otherwise false statement.
+     * Returns the index position of an array. If the value provided
+     * in the array,then
      *
-     * @function x.indexOf(array , n )
+     * @function x.indexOf(array, val)
      * @params {number} array The array value in x.indexOf.
      * @params {number} val The value which needs to be searched for its position.
      * @returns {number} If number of elements to be fecthed is provided
@@ -136,20 +133,20 @@ this.load = function load(x) {
      * is not provided return false statement
      *
      * @example
-     * var x=[1,2,3,4,5,6];
-     * console.log(x.indexOf(x,3));  // Prints True
+     * var indxArray = [1,2,3,4,5,6];
+     * console.log(x.indexOf(indxArray, 3));  // Prints True
      *
      * @version 1.0
      */
-    x.indexOf = Array.indexOf || function indexOf(array, val) {
+    x.indexOf = function indexOf(array, val) {
         var i,
             iLen = array.length;
 
         if (array.indexOf) {
-            return array.indexOf(n);
+            return array.indexOf(val);
         }
 
-        for(i=0; i<iLen; i += 1) {
+        for(i=0; i < iLen; i += 1) {
             if (array[i] === val) {
                 return i;
             }
@@ -159,30 +156,30 @@ this.load = function load(x) {
 
     /**
      * Returns the index value of an array. If the value provided in the array,
-     * then return a Position of that element.
+     * then return a statement True or False.
      *
-     * @function x.exists(array, n)
-     * @params {number} array The array value in x.indexValue.
-     * @params {number} n The  n number is to find the value in x.indexValue.
-     * @returns {Array , value} If number of elements to be fecthed is provided
-     * then returns a position of n number of an element .If n number
-     * is not provided return -1 (which means number is not in the array)
+     * @function x.exists(array, val)
+     * @params {number} array The array value in x.exists.
+     * @params {number} val The value which needs to be searched for its statement.
+     * @returns {number} If number of elements to be fecthed is provided
+     * then returns a statement of an array either True or False. If n number
+     * is not provided return -1 (which means number is not in the array).
      *
      * @example
-     * var arr = [1,2,3,4,5,6];
-     * console.log(x.indexValue(arr, 3));  // Prints 2
+     * var extArray = [1,2,3,4,5,6];
+     * console.log(x.indexValue(extArray, 3));  // Prints 2
      *
      * @version 1.0
      */
     x.exists = function exists(array, val) {
+        var i,
+            iLen = array.length;
 
-        var i;
-
-            for(i = 0; i < array.length; i++) {
-                if(array[i] === val) {
-                    return true;
-                }
+        for(i = 0; i < iLen; i++) {
+            if(array[i] === val) {
+                return true;
             }
+        }
             return false;
     };
 
@@ -191,29 +188,28 @@ this.load = function load(x) {
      * Returns the index of the last occurrence of value in the array.
      * If the value provided in the array,then return a last Position of that element.
      *
-     * @function x.lastIndexOf(array , n )
+     * @function x.lastIndexOf(array, val)
      * @params {number} array The array value in x.lastIndexOf.
-     * @params {number} n The  n number is to find the value in x.lastIndexOf.
-     * @returns {Array , value} If number of elements to be fecthed is provided
-     * an having a dublicate value in the array then returns a last position of n number
+     * @params {number} val The value which needs to be searched for its last position.
+     * @returns {number} If number of elements to be fecthed is provided
+     * an having a dublicate value in that array then returns a last position of the number
      * of an element. If n number is not provided return -1
      * (which means number is not in the array)
      *
      * @example
-     * var x=[1,2,3,4,5,6,2,3,4,5];
-     * console.log(x.lastIndexOf(x,3));  // Prints 7
+     * var lstInArray = [1,2,3,4,5,6,2,3,4,5];
+     * console.log(x.lastIndexOf(lstInArray, 3));  // Prints 7
      *
      * @version 1.0
      */
+    x.lastIndexOf = function lastIndexOf(array, val) {
+        var retArr = [],
+            i,
+            iLen = array.length;
 
-    x.lastIndexOf = function lastIndexOf(array,n) {
-
-        var retArr=[],
-            i;
-
-            for(i = 0; i < array.length; i++) {
-                if(array[i] === n) {
-                    return array.lastIndexOf(n);
+        for(i = 0; i < iLen; i++) {
+            if(array[i] === val) {
+                return array.lastIndexOf(val);
                 }
             }
             return -1;
@@ -224,25 +220,25 @@ this.load = function load(x) {
      * Uses a binary search to determine the index at which the value should
      * be inserted into the list in order to maintain the list's sorted order
      *
-     * @function x.sortedIndex(array , n )
+     * @function x.sortedIndex(array, val)
      * @params {number} array The array value in x.sortedIndex.
-     * @params {number} n The  n number is to find the value in x.sortedIndex.
+     * @params {number} val The value which needs to be searched for its position.
      * @returns {Array , value} If number of elements to be fecthed is provided
-     * then return the position of n number of an element. If n number
+     * then return the position of the number of an element. If n number
      * is not provided return the length of that array.
      *
      * @example
-     * var x=[10,20,30,40,50,60];
-     * console.log(x.sortedIndex(x,35));  // Prints 3
+     * var sorInArray = [10,20,30,40,50,60];
+     * console.log(x.sortedIndex(sorInArray, 35));  // Prints 3
      *
      * @version 1.0
      */
-    x.sortedIndex = function sortedIndex (array, n) {
+    x.sortedIndex = function sortedIndex (array, val) {
+        var i,
+            iLen = array.length;
 
-        var i;
-
-        for(i = 0; i < array.length; i++) {
-            if(array[i] >= n) {
+        for(i = 0; i < iLen; i++) {
+            if(array[i] >= val) {
                 return i;
             }
 
@@ -252,22 +248,21 @@ this.load = function load(x) {
 
     /**
      * Converts arrays into objects. if the value is passed then,
-     * it merged the value to that object
+     * it merged the value to that object.
      *
-     * @function x.merge(array1, array2 )
+     * @function x.merge(array1, array2)
      * @params {number} array1 The array1 value in x.merge.
-     * @params {number} array2 The  array2 is to merged the value in x.merge.
+     * @params {number} array2 The array2 is to merged the value in x.merge.
      * @returns {Array1 , Array2} If number of elements array2 is passed then it merged the
      * value with an array1. If array2 value is not provided returns the Error.
      *
      * @example
-     * var x=['moe', 'larry', 'curly'];
-     * console.log(x.merge(x,30,40,50));  //Prints {'moe': 30,'larry': 40,'curly': 50}
+     * var mergArray = ['moe', 'larry', 'curly'];
+     * console.log(x.merge(mergArray, 30, 40, 50));  //Prints {'moe': 30,'larry': 40,'curly': 50}
      *
      * @version 1.0
      */
     x.merge = function merge(array1, array2) {
-
         var result = {},
             i,
             iLen = array1.length;
@@ -285,44 +280,41 @@ this.load = function load(x) {
     /**
      * Returns everything but the last entry of the array is removed.
      *
-     * @function x.initial(array, n, n1 )
+     * @function x.initial(array, n, val)
      * @params {number} array The array value in x.initial.
-     * @params {number} n The  n number having null value in x.initial.
-     * @params {number} n1 The n number value in x.initial.
-     * @returns {Array ,n, n1} If number of elements n is passed then it return everything
-     * but last array is removed.If n number value is not provided it return the empty array.
+     * @params {number} n The  n number having value in x.initial.
+     * @params {number} val The value which needs to be searched to remove the Last entry.
+     * @returns {Array, number} If number of an elements is passed then it return everything
+     * but last array is removed. If n number value is not provided it return the empty array.
      *
      * @example
-     * var x=[5,4,3,2,1];
-     * console.log(x.initial(x,1));  //Prints [5,4,3,2]
+     * var initArray = [5,4,3,2,1];
+     * console.log(x.initial(initArray, 1));  //Prints [5,4,3,2]
      *
      * @version 1.0
      */
-    x.initial = function(array, n, n1) {
-        return array.slice.call(array, 0, array.length - ((n === null) || n1 ? 1 : n));
+    x.initial = function(array, n, val) {
+        return array.slice.call(array, 0, array.length - ((n === null) || val ? 1 : n));
     };
 
     /**
      * Returns everything but the first entry of the array is removed.
      *
-     * @function x.rest(array, n, n1 )
+     * @function x.rest(array, n, val)
      * @params {number} array The array1 value in x.rest.
      * @params {number} n The  n number having null value in x.initial.
-     * @params {number} n1 The n number value in x.initial.
+     * @params {number} val The value which needs to be searched to remove the First entry.
      * @returns {Array ,n,n1} If number of elements n is passed then it return everything
      * but first array is removed.If n number value is not provided it return the empty array.
      *
      * @example
-     * var x=[5,4,3,2,1];
-     * console.log(x.rest(x,1));  //Prints [4,3,2,1]
+     * var rstArray = [5,4,3,2,1];
+     * console.log(x.rest(rstArray, 1));  //Prints [4,3,2,1]
      *
      * @version 1.0
      */
-
     x.rest = function(array, n, n1) {
-
         return array.slice.call(array ,n === null || n1 ? 1 : n);
-
     };
 
     /**
@@ -336,18 +328,18 @@ this.load = function load(x) {
      * If array having all falsy value it return the empty array.
      *
      * @example
-     * var x=[0, 1, false, 2, '', 3];
+     * var compArray = [0, 1, false, 2, '', 3];
      * console.log(x.compact(x));  //Prints [1,2,3]
      *
      * @version 1.0
      */
-
     x.compact = function compact (array) {
         var retArr=[],
-           i;
+            i,
+            iLen = array.length;
 
-        for(i=0; i < array.length; i++) {
-            if(array[i]){
+        for(i=0; i < iLen; i++) {
+            if(array[i]) {
                 retArr.push(array[i]);
             }
         }
@@ -360,23 +352,25 @@ this.load = function load(x) {
      *
      * @function x.without(array, number)
      * @params {number} array The array1 value in x.without.
-     * @params {number} number The number is to find the value in x.without.
+     * @params {number} number The value which needs to be searched
+     * to remove the dublicates values.
      * @returns {Array, value} If number of elements to be fecthed is provided
      * then remove the repeated number an return the number of elements.
      * If n number value is not provided it return all the number also the dublicates numbers.
      *
      * @example
-     * var x= [1, 2, 1, 0, 3, 1, 4];
-     * console.log(x.without(x,1,0));  //Prints [2,3,4]
+     * var witArray = [1, 2, 1, 0, 3, 1, 4];
+     * console.log(x.without(witArray, 1, 0));  //Prints [2,3,4]
      *
      * @version 1.0
      */
-
-
     x.without =function without(array,number) {
-        for(var i = 0; i < array.length; i++){
-             for(var j = 1; j < arguments.length; j++){
-                if(array[i] === arguments[j]){
+        var i,j,
+            iLen = array.length;
+
+        for(i = 0; i < iLen; i++) {
+            for(j = 1; j < arguments.length; j++) {
+                if(array[i] === arguments[j]) {
                     array.splice(i,1);
                 }
             }
@@ -389,23 +383,21 @@ this.load = function load(x) {
      * corresponding position.if the value is passed then,it merged the value
      * to that object
      *
-     * @function x.zip(array1, array2,array3 )
+     * @function x.zip(array1, array2, array3)
      * @params {number} array1 The array1 value in x.zip.
      * @params {number} array2 The array2 is to merged the value in x.zip.
      * @params {number} array2 The array3 is to merged the value in x.zip.
-     * @returns {Array1 , Array2, Array3} If number of elements array2,array3 is passed then
+     * @returns {Array2, Array3} If number of elements array2,array3 is passed then
      * it merged the value with an array1. If array2 value is not provided returns the Error.
      *
      * @example
-     * var x=['moe', 'larry', 'curly'];
-     * console.log(x.zip(x,[30,40,50].[true,false,false]));
+     * var zpArray = ['moe', 'larry', 'curly'];
+     * console.log(x.zip(zpArray, [30,40,50], [true,false,false]));
      * //Prints {'moe': 30, true ,'larry': 40, false ,'curly': 50, false}
      *
      * @version 1.0
      */
-
-     x.zip = function zip (array1, array2,array3) {
-
+    x.zip = function zip (array1, array2,array3) {
         var result = {},
             i,
             iLen = array1.length;
@@ -418,6 +410,37 @@ this.load = function load(x) {
             result [array1[i]] = [array2[i],array3[i]];
         }
         return result;
+    };
+
+    /**
+     * Flattens a nested array
+     *
+     * @function x.flatten(array, val)
+     * @params {number} array The array value in x.flatten.
+     * @params {number} val The val is the value in x.flatten
+     * @returns {Array} If array is flatten then remove the flatten from the array and
+     * return the number in an array format. If array value is not provided returns the Error.
+     *
+     * @example
+     * var flatArray =  [[1, 2], [3, 4], [5, [6, [7, 8]]]];
+     * var retArray = [];
+     * x.flatten(flatArray, retArray);
+     * console.log(b); // Prints [1,2,3,4,5,6,7,8]
+     *
+     * @version 1.0
+     */
+    x.flatten = function flatten (array,val) {
+        var i,
+            iLen = array.length;
+
+        if(iLen !== undefined) {
+            for (i=0; i < iLen; i++) {
+                 x.flatten(array[i],val);
+            }
+        }
+        else {
+             val.push(array);
+        }
     };
 
 };
--- tests/simple-tests.js
+++ tests/simple-tests.js
@@ -5,36 +5,39 @@ var x = require("../src/x").x;
 //load other x modules
 require('../src/x.array').load(x);
 
-var a = [1,2,3,4,5,6],
-    array1 = [1,2,3,4,5,6],
-    array2 = [1,4,6,8,9,10],
-    //b=[1,2,3,4,5,6],
-    c=[1,2,8,5,6,48,45,4],
-    d=[1,2,3,4,5,6,4,2,3,5],
-    e=[10,20,30,40,50,60];
-    f=['moe', 'larry', 'curly'],
-    d=[5,4,3,2,1],
-    e=[5,4,3,2,1],
-    h=[0, 1, false, 2, '', 3],
-    i=[1, 2, 1, 0, 3, 1, 4],
-    g=['moe', 'larry', 'curly'];
+var firArray   = [1,2,3,4,5,6],
+    lstArray   = [1,2,3,4,5,6],
+    intrArray1 = [1,2,3,4,5,6],
+    intrArray2 = [1,4,6,8,9,10],
+    indxArray  = [1,2,3,4,5,6,7,8],
+    extArray   = [1,2,8,5,6,48,45,4],
+    lstInArray = [1,2,3,4,5,6,4,2,3,5],
+    srtInArray = [10,20,30,40,50,60],
+    mergArray  = ['moe', 'larry', 'curly'],
+    initArray  = [5,4,3,2,1],
+    rstArray   = [5,4,3,2,1],
+    compArray  = [0, 1, false, 2, '', 3],
+    witArray   = [1, 2, 1, 0, 3, 1, 4],
+    zpArray    = ['moe', 'larry', 'curly'];
+    flatArray = [[1, 2], [3, 4], [5, [6, [7, 8]]]], retArray = [];
 
 
 
 
-
-console.log(x.range(0,5,2));
-console.log(x.first(a,1));
-console.log(x.last(a,2));
-console.log(x.intersection(array1,array2));
-console.log(x.indexOf(a,7));
-console.log(x.indexValue(c,5));
-console.log(x.lastIndexOf(d,3));
-console.log(x.sortedIndex(e,55));
-console.log(x.merge(f,[30,40,50]));
-console.log(x.initial(d,1));
-console.log(x.rest(e,1));
-console.log(x.compact(h));
-console.log(x.without(i,1,0));
-console.log(x.zip(g,[30,40,50],[true,false,false]));
+console.log(x.range(0, 5, 2));
+console.log(x.first(firArray, 1));
+console.log(x.last(lstArray, 2));
+console.log(x.intersection(intrArray1, intrArray2));
+console.log(x.indexOf(indxArray, 6));
+console.log(x.exists(extArray, 5));
+console.log(x.lastIndexOf(lstInArray, 3));
+console.log(x.sortedIndex(srtInArray, 55));
+console.log(x.merge(mergArray, [30, 40, 50]));
+console.log(x.initial(initArray, 1));
+console.log(x.rest(rstArray, 1));
+console.log(x.compact(compArray));
+console.log(x.without(witArray, 1, 0));
+console.log(x.zip(zpArray, [30,40,50], [true,false,false]));
+x.flatten(flatArray,retArray);
+console.log(retArray);
 
