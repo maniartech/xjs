ec37d6c02ac24f2f108db8c199f92582d7d94f5c
Aamir Maniar <aamironline@gmail.com>
2013-06-20 22:17:50 +0530

    #1: Setup grunt and correct project structure...

---
+++ Gruntfile.js
@@ -0,0 +1,66 @@
+/**
+ * framework-factory
+ * http://framework-factory.com/
+ *
+ * Copyright (c) 2013 Maniar Technologies Private Limited
+ * Author Mohamed Aamir Maniar
+ * Licensed under the MIT license.
+ * https://framework-factory.com/LICENSE-MIT
+ *
+ **/
+
+'use strict';
+
+var src = [
+        'src/x.js',
+        'src/x.is.js',
+        'src/x.array.js',
+        'src/x.objects.js'
+    ];
+
+module.exports = function (grunt) {
+
+    // Project configuration.
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        license: grunt.file.read('src/license.js'),
+        concat: {
+            options: {
+                //separator: ';',
+                banner: '<%= license %>'
+            },
+            main: {
+                src: src,
+                dest: 'lib/x.js'
+            }
+        },
+        uglify: {
+            options: {
+                banner: '<%= license %>'
+            },
+            main: {
+                files: {
+                    'lib/x.min.js': ['lib/x.js']
+                }
+            }
+        },
+        watch: {
+            scripts: {
+                files: ['src/**/*.js'],
+                tasks: ['concat'],
+                options: {
+                    nospawn: true
+                }
+            }
+        }
+    });
+
+    //Load plugins
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+
+
+    // Default task(s).
+    grunt.registerTask('default', ['concat:main', 'uglify:main']);
+};
