687284b6e5969959dad60657e5b4de1ea5ac652c
Aamir Maniar <aamironline@gmail.com>
2013-06-22 09:21:43 +0530

    #1: Setup grunt and correct project structure

--- src/license.js
+++ src/license.js
@@ -6,5 +6,14 @@
  *       /_/|_|_/ /____/
  *           /___/
  *
+ *      version: 1.0
+ *      x.js - The most complete JavaScript library of its own kind.
+ *
+ *      http://www.x-js.net
+ *      License: http://www.x-js.net/license
+ *
+ *      Copyright (c) 2013 - Maniar Technologies Private Limited - India
+ *      http://www.maniartech.com
+ *      Twitter: @maniartech
  *
  */
